d69911285138f8d1ac7f41c750cb986e2fd431d6
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2025-02-16 06:55:41 -0500

    ADD raw layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
+import { Gupter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+
+const gupter = Gupter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Sucky Design?",
@@ -13,7 +20,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={gupter.className}>
+        <div className="flex justify-between items-center p-4 m-2">
+          <h1>
+            <Link href="/">Sucky Design?</Link>
+          </h1>
+          <div>
+            <Link href="https://its-ours.org?by=sucky-design" target="_blank">
+              its-ours.org
+            </Link>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
